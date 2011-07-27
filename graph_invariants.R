30cdf24964c43ef75147537b043d74381da85a62
Andrej Kastrin <andrej.kastrin@yahoo.com>
2011-07-27 09:06:26 +0200

    Add base program flow.

--- graph_invariants.R
+++ graph_invariants.R
@@ -1,12 +1,18 @@
-setwd("~/Desktop/fb_graph/data_07-25-2011")
+setwd("~/Desktop/fb_graph")
 
 require(igraph)
 
-file.name <- c("test2.net")
-awk.str <- "awk \'{if(NR==1)sub(/^\xef\xbb\xbf/, \"\"); print}\'"
-awk.str <- paste(awk.str, file.name)
+data.dir <- "data_07-25-2011"
+my.files <- dir(path=data.dir)
 
-pipe.con <- pipe(awk.str, encoding="UTF-8")
-my.graph <- read.graph(file=pipe.con, format="pajek")
+GetInvariate <- function(file.name) {
+  awk.str <- "awk \'{if(NR==1)sub(/^\xef\xbb\xbf/, \"\"); print}\'"
+  #   file.nobom <- paste(awk.str, file.name)
+  file.nobom  <- paste(awk.str, " ", "./", data.dir, "/", file.name, sep="")
+  pipe.con <- pipe(description=file.nobom, encoding="UTF-8")
+  my.graph <- read.graph(file=pipe.con, format="pajek")
+  my.diam <- diameter(graph=my.graph, directed=FALSE)
+  return(my.diam)
+}
 
-summary(my.graph)
+bla <- sapply(X=my.files, FUN=GetInvariate)
